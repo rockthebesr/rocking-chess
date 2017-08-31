fdf95d43ef098e5cb2519136554ea73c0bb1ead4
Luo Rock <luorock@LuodeMacBook-Air.local>
2017-08-30 20:10:05 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+var port = process.env.PORT || 3000;
+/**
+ * Set up a basic Express server.
+ */
+app.set('port', port);
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/views'));
+
+/**
+ * Routes
+ */
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/local', function(req, res) {
+    res.sendFile(__dirname + '/public/views/local.html')
+});
+
+app.get('/ai', function(req, res) {
+    res.sendFile(__dirname + '/public/views/ai.html')
+});
+
+server.listen(port, function() {
+    console.log('Express server listening.');
+});
