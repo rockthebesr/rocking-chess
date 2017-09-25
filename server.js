b8f24bcb5dfecfa354fbf1c40235752469b03872
Luo Rock <luorock@LuodeMacBook-Air.local>
2017-09-25 01:06:11 -0700

    start working on multiplayer part. updated some socket usage

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var http = require('http');
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 var port = process.env.PORT || 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 /**
  * Set up a basic Express server.
  */
@@ -20,39 +24,53 @@ app.use(express.static(__dirname + '/public/views'));
  * Routes
  */
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.render(__dirname + '/public/index');
 });
 
 app.get('/local', function(req, res) {
-    res.sendFile(__dirname + '/public/views/local.html');
+    res.render(__dirname + '/public/views/local');
 });
 
 app.get('/ai', function(req, res) {
-    res.sendFile(__dirname + '/public/views/ai.html');
+    res.render(__dirname + '/public/views/ai');
 });
 
-app.get('/room', function(req, res) {
-    res.sendFile(__dirname + '/public/views/againstOther.html');
+app.get('/room', function(req, res){
+    var roomId = Math.floor(Math.random() * 99999);
+    res.redirect('/room/' + roomId);
+})
+app.get('/:roomId', function(req, res) {
+    res.render(__dirname + '/public/views/againstOther', {"roomId": req.params.roomId});
 });
 
 /**
  * Socket io
  */
-var connectionCounts = 0;
+var socketIdToRoom = {};
+var roomCounts = {}
 var roomId = null;
 io.on('connection', function (socket) {
 
-    connectionCounts++
-    io.emit('counts', connectionCounts);
 
-    // Join socket to random room on connect
-    roomId = Math.floor(Math.random() * 99999);
-    socket.join(roomId);
-
-    // Tell the socket that it is in a room
-    io.to(roomId).emit('assignRoom', roomId);
-
-  
+    //socket has a room id to join
+    socket.on('askToJoin', function(roomId){
+        socketIdToRoom[socket.id] = roomId;
+        var type;
+        if (roomCounts[roomId] == null || roomCounts[roomId] == undefined) {
+            
+        socket.join(roomId);
+            type = 0;
+            roomCounts[roomId] = 1;
+        } else if (roomCounts[roomId] == 2) {
+            io.to(roomId).emit("rejected", "Room already has 2 players!");
+        } else {
+            socket.join(roomId);
+            type = 1;
+            roomCounts[roomId] += 1;
+        }
+        io.to(socket.id).emit('confirmJoin', {roomId: roomId, type: type});
+        io.to(roomId).emit('roomCount', roomCounts[roomId]);
+    })
 
     //Tell socket to leave a room
     socket.on('leaveRoom', function(oldRoomId) {
@@ -70,10 +88,10 @@ io.on('connection', function (socket) {
     });
 
     socket.on('disconnect', () => {
-      connectionCounts--;
-      io.sockets.emit('counts', connectionCounts);
+        var room = socketIdToRoom[socket.id];
+        roomCounts[room]--;
+        io.to(room).emit('roomCount', roomCounts[roomId]);
     });
-
   });
 
 server.listen(port, function() {
